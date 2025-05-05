fa33e8bdae65034812f5b464d19212d0ef212656
Temistakes <temistakes@yandex.ru>
2025-05-05 16:31:56 +0300

    commit #5

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Libraries
 
+import $ from "jquery";
+
 import "./modules/sliders.js";
 import "./modules/modals.js";
 import "./modules/phone-mask.js";
@@ -8,4 +10,10 @@ import "./modules/card.js";
 import "./modules/menu.js";
 import "./modules/redirect.js";
 import "./modules/files.js";
+
 import fslightbox from "fslightbox";
+
+const header = $(".header");
+const main = $(".main");
+
+main.css("padding-top", `${header.height() + parseInt(header.css("margin-bottom"))}px`);
