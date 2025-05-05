08bdae07f5de29a5cfd1ef6665dfdbbfe278be7b
Temistakes <temistakes@yandex.ru>
2025-05-05 21:09:39 +0300

    commit #6

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -4,3 +4,10 @@ $(".menu").on("click", (e) => {
     $(e.currentTarget).toggleClass("menu_active");
     $(".mobile-menu").slideToggle(300);
 });
+
+$(".filter-item__open").on("click", (e) => {
+    const hidden = $(e.currentTarget).next();
+    hidden.slideToggle(300);
+    $(e.currentTarget).toggleClass("active");
+    $(e.currentTarget).parent().toggleClass("active");
+});
